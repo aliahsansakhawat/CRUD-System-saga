b3fc6dd226989b023a3319463d616a0cef731f37
aliahsansakhawat <mianaliahsan3@gmail.com>
2025-06-10 12:22:45 +0500

    404 Error resolve method

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Create from "./Create";
+
 import Read from "./Read";
 import Update from "./Update";
 import Delete from "./Delete";
+import Create from "./Create";
 const App = () => {
   return (
     <div>
