a4b8a019c846c4de456bca606806ddc6fe59909c
aliahsansakhawat <mianaliahsan3@gmail.com>
2025-06-10 11:58:01 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Create from "./create";
+import Create from "./Create";
 import Read from "./Read";
 import Update from "./Update";
 import Delete from "./Delete";
